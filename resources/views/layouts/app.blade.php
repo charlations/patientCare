195153a67f24b2a5979856278e73ed7b06d64688
charlations <carla.herrera.rendon@gmail.com>
2019-01-14 17:13:24 -0600

    small changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,7 @@
     <!-- Fonts -->
     <link rel="dns-prefetch" href="https://fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet" type="text/css">
+		<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 
     <!-- Styles -->
 		<link href="{{ asset('css/bootstrap/bootstrap.css') }}" rel="stylesheet"> <!--load all styles -->
