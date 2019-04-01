149cd340b20c8f1ba78ffb5f3b40413ecdec6d56
charlations <carla.herrera.rendon@gmail.com>
2019-04-01 13:09:42 -0600

    Ya acabé tests

--- tests/Feature/RoutesTest.php
+++ tests/Feature/RoutesTest.php
@@ -48,9 +48,10 @@ class RoutesTest extends TestCase
 	* Test User reachable routes are reachable. 
 	*
 	*/
-	public function testUserRoutes($email = null, $pass = null) {
+	public function testUserRoutes($email = null, $pass = null, $role = 'admin') {
 		$email = ($email == null ? 'carla@email.com' : $email);
 		$pass = ($pass == null ? '123456' : $pass);
+		$user = new User(['email' => $email, 'password' => $pass]);
 		$response = $this->post('/login', [
 			'email' => $email,
 			'password' => $pass,
@@ -62,34 +63,49 @@ class RoutesTest extends TestCase
 		$response->assertStatus(200);
 		$response->assertLocation('/');
 
-		$response = $this->get('/home');  //get('/home');
-		//echo $response->getOriginalContent();
-		print_r($response->headers);
+		$response = $this->get('/home');
 		$response->assertStatus(200);
 		$response->assertViewIs('patient.index');
 
 		$response = $this->get('/insurance');
-		$response->assertStatus(200);
-		$response->assertViewIs('insurance.index');
-
+		if($role == 'admin' || $role == 'doctor' || $role == 'assistant') {
+			$response->assertStatus(200);
+			$response->assertViewIs('insurance.index');
+		} else {
+			echo ("\nNo tiene permiso\n");
+			$response->assertStatus(302);
+		}
+		
 		$response = $this->get('/user');
-		$response->assertStatus(200);
-		$response->assertViewIs('user.index');
+		if($role == 'admin') {
+			$response->assertStatus(200);
+			$response->assertViewIs('user.index');
+		} else {
+			$response->assertStatus(302);
+		}
 
 		$response = $this->get('/patient');
-		$response->assertStatus(200);
-		$response->assertViewIs('patient.index');
+		if($role == 'admin' || $role == 'doctor' || $role == 'assistant') {
+			$response->assertStatus(200);
+			$response->assertViewIs('patient.index');
+		} else {
+			$response->assertStatus(302);
+		}
 
 		$response = $this->get('/appointment');
-		$response->assertStatus(200);
-		$response->assertViewIs('appointment.index');
+		if($role == 'admin' || $role == 'doctor') {
+			$response->assertStatus(200);
+			$response->assertViewIs('appointment.index');
+		} else {
+			$response->assertStatus(302);
+		}
 	}
 
-	/*public function testDoctorRoutes() {
-		testUserRoutes('chezz@email.com');
+	public function testDoctorRoutes() {
+		$this->testUserRoutes('chezz@email.com', '654321', 'doctor');
 	}
 
 	public function testAssistantRoutes() {
-		testUserRoutes('azu@email.com');
-	}*/
+		$this->testUserRoutes('azu@email.com', '123456', 'assistant');
+	}
 }
